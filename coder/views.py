27b226f603da2e9a79d8335bb690a6ac4de221cf
Arth Tyagi <arthtyagi7@gmail.com>
2020-07-15 19:06:21 +0530

    added new templates, configuring views

--- coder/views.py
+++ coder/views.py
@@ -3,9 +3,22 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, RedirectView
 from django.db.models import Q
+from .models import Question, Answer
 
 from .forms import ResultForm
 
 def home(request):
-    return render(request, 'coder/coder.html', {'title': 'Home'})
+    return render(request, 'coder/coder_list.html', {'title': 'Home'})
 
+class CoderListView(ListView):
+    model = Question
+    template_name = "coder/coder_list.html"
+
+class CoderDetailView(DetailView):
+    model = Question
+    template_name = "coder/coder_detail.html"
+    
+class CoderCreateView(CreateView):
+    model = Answer
+    template_name = "coder/coder_form.html"
+    
